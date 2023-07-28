3ad83a342d0d7cee129a0135ad5b678b2df51c13
Zafion <zafion@gmail.com>
2023-07-28 13:55:51 +0200

    Actualizados varios
    Android manifest, colors y otros

--- app/src/main/java/com/example/gameshelf/ui/theme/Color.kt
+++ app/src/main/java/com/example/gameshelf/ui/theme/Color.kt
@@ -5,4 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
+val Red100 = Color(0xFFEE5449)
+val Red600 = Color(0xFFC2342A)
+val Yellow600 = Color(0xFFFFC107)
 val Teal200 = Color(0xFF03DAC5)
